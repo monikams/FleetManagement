1936775eef654b1310ad091a58dcafda15228ce1
monika_spasova <monikaspasova1@gmail.com>
2018-10-20 16:20:26 +0300

    Added table TelematicsData

---
+++ Database/FleetManagement/dbo/Tables/TelematicsData.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE [dbo].[TelematicsData] (
+    [Id] UNIQUEIDENTIFIER PRIMARY KEY CONSTRAINT [DF_TelematicsData_Id] DEFAULT (newsequentialid()) NOT NULL,
+	[Vehicle_Id] UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Vehicle(Id) NOT NULL,
+	[CurrentSpeed] INT NULL,
+    [FuelLevel]   INT NULL,
+    [FuelUsed] INT  NULL, 
+    [Longitude] FLOAT NULL, 
+    [Latitude] FLOAT NULL, 
+    [RPM] INT NULL, 
+    [EngineSpeed] INT NULL, 
+    [EngineHours] INT NULL, 
+    [Mileage] INT NULL, 
+    [IsDoorOpen] BIT NULL, 
+    [Idling] TIME NULL, 
+    [Ignition] BIT NULL, 
+    [SeatBelt] BIT NULL, 
+    [HarshAccelaration] BIT NULL, 
+    [HarshBraking] BIT NULL, 
+    [HarshCornering] BIT NULL, 
+    [TailGating] BIT NULL,
+);
+
+	
--- Database/FleetManagement/dbo/Tables/Vehicle.sql
+++ Database/FleetManagement/dbo/Tables/Vehicle.sql
@@ -1,7 +1,7 @@
 ﻿CREATE TABLE [dbo].[Vehicle] (
     [Id]          UNIQUEIDENTIFIER PRIMARY KEY CONSTRAINT [DF_Vehicle_Id] DEFAULT (newsequentialid()) NOT NULL,
     [Driver_Id]   UNIQUEIDENTIFIER NULL,
-	[Company_Id]   UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Company(Id) NULL,
+	[Company_Id]  UNIQUEIDENTIFIER FOREIGN KEY REFERENCES Company(Id) NULL,
     [VIN]         NVARCHAR (256)   NOT NULL,
     [PlateNumber] NVARCHAR (16)    NOT NULL,
 	[Type] [int] NOT NULL,
