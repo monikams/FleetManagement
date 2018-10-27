468b87f0096b0144f7ff6088c292b50cb4800610
monika_spasova <monikaspasova1@gmail.com>
2018-10-27 18:47:59 +0300

    Added custom username

--- FleetManagement/WebApiService/Controllers/AccountController.cs
+++ FleetManagement/WebApiService/Controllers/AccountController.cs
@@ -330,7 +330,7 @@ namespace WebApiService.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new User() { UserName = model.Email, Email = model.Email };
+            var user = new User() { UserName = model.Username, Email = model.Email };
 
             IdentityResult result = await UserManager.CreateAsync(user, model.Password);
 
