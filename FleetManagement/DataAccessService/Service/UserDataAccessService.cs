58e3b006fea8aa0bd6611ae8950b1a8e82f46c48
monika_spasova <monikaspasova1@gmail.com>
2018-10-28 14:55:17 +0200

    Implemented UserDataAccessService

---
+++ FleetManagement/DataAccessService/Service/UserDataAccessService.cs
@@ -0,0 +1,40 @@
+﻿namespace DataAccessService.Service
+{
+    using System.Data.Entity;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using AutoMapper;
+
+    using Data;
+
+    using DataAccessService.Models;
+
+    public class UserDataAccessService : IUserDataAccessService
+    {
+        private readonly MapperConfiguration _config;
+
+        private readonly FleetManagementDbContext _context = new FleetManagementDbContext();
+
+        private readonly IMapper _mapper;
+
+        public UserDataAccessService()
+        {
+        }
+
+        public UserDataAccessService(FleetManagementDbContext context)
+        {
+            this._context = context;
+            this._config = new MapperConfiguration(cfg => { cfg.CreateMap<User, Data.Models.User>().ReverseMap(); });
+            this._mapper = new Mapper(this._config);
+        }
+
+        public async Task<User> ByUsername(string username)
+        {
+            var user = await this._context.Users.Where(u => u.UserName == username).FirstOrDefaultAsync();
+            var mappedUser = this._mapper.Map<Data.Models.User, User>(user);
+
+            return await Task.Run(() => mappedUser);
+        }
+    }
+}
