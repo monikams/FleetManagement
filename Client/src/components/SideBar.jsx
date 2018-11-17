881ad059fd303a5dcbe6e3d99427c9eb70eacff4
monika_spasova <monikaspasova1@gmail.com>
2018-11-17 17:43:48 +0200

    Sidebar active item update

--- Client/src/components/SideBar.jsx
+++ Client/src/components/SideBar.jsx
@@ -6,22 +6,25 @@ import { withRouter } from 'react-router'
 class SideBar extends React.Component {
 
   constructor(props) {
-    super(props)
+    super(props);
+    this.state = {
+      active: 'Companies',
+    }
   }
 
-    handleItemClick = (event) => {
-      const { target : { textContent } } = event;
-      this.props.router.push(textContent.toLowerCase());
-    }
+  handleItemClick = (event) => {
+    const { target : { textContent } } = event;
+    this.props.router.push(textContent.toLowerCase());
+    this.setState({ active: textContent });
+  }
 
-    renderItem = (text, index) => (
-       <div key={index} onClick={this.handleItemClick} className='item'>
-          <div className='itemText'>
-              <span className='titleClass'>{text}</span>
-          </div>
-          <div className='selectedIndicator'></div>
-       </div>
-    );
+  renderItem = (text, index) => (
+      <div key={index} onClick={this.handleItemClick} className={text === this.state.active ? 'selectedIndicator' : 'item'}>
+        <div className='itemText'>
+            <span className='titleClass'>{text}</span>
+        </div>
+      </div>
+  );
  
   render() {
     const { id } = this.props;
