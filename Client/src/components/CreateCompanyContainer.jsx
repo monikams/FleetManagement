948ef959bcc9d435eed259f0ae2cbd2a99416d98
monika_spasova <monikaspasova1@gmail.com>
2018-11-18 12:39:45 +0200

    Added basic form fields in Create Comany

--- Client/src/components/CreateCompanyContainer.jsx
+++ Client/src/components/CreateCompanyContainer.jsx
@@ -5,16 +5,29 @@ import CompaniesStore from '../stores/CompaniesStore';
 import CompaniesActions from '../actions/CompaniesActions.js';
 import connectToStores from 'alt-utils/lib/connectToStores';
 import { withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
-  root: {
-    width: '100%',
-    marginTop: theme.spacing.unit * 3,
-    overflowX: 'auto',
+  button: {
+    marginTop: '20px',
+    width: '112px',
+    height: '40px',
   },
-  table: {
-    minWidth: 700,
+  container: {
+    width: '75%',
+    margin: 'auto',
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  form: {
+    width: '50%',
+    margin: 'auto',
+    display: 'grid',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
   },
 });
 
@@ -34,13 +47,90 @@ class CreateCompanyContainer extends Component {
         CompaniesActions.loadCompanies();
     }
 
+    handleChange = name => event => {
+        // const { target: { value }} = event;     
+        // const { localUser } = this.state;
+        // const newUser = merge(localUser, { [name]: value });
+        // this.setState({
+        //     localUser: newUser
+        // });
+    };
+
+    handleSaveButtonClick = () => {
+        //AuthorizationActions.registerUser(this.state.localUser);
+    }
+
     render() {      
         const { companies, classes } = this.props;
     
         return (
-            <div>
-               Hello
-            </div>
+            <div className={classes.form} >  
+                <div className={classes.container} >
+                    <TextField
+                        required
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        id="name"
+                        label="Name"
+                        placeholder="Enter company`s name"
+                        className={classes.textField}          
+                        onChange={this.handleChange('name')}
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        id="email"
+                        label="Email"
+                        placeholder="Enter company`s email"
+                        className={classes.textField}          
+                        onChange={this.handleChange('email')}
+                        margin="normal"
+                    />
+                    <TextField
+                        required
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        id="address"
+                        label="Address"
+                        placeholder="Enter company`s address"
+                        className={classes.textField}         
+                        onChange={this.handleChange('address')}
+                        margin="normal"
+                    />
+                     <TextField
+                        fullWidth
+                        InputLabelProps={{
+                            shrink: true,
+                        }}
+                        id="phone"
+                        label="Phone"
+                        placeholder="Enter company`s phone number"
+                        className={classes.textField}         
+                        onChange={this.handleChange('phone')}
+                        margin="normal"
+                    />
+                </div>
+                <div className={classes.container} >
+                    <Button 
+                        variant="contained" 
+                        size="large" 
+                        color="primary" 
+                        className={classes.button}
+                        onClick={this.handleSaveButtonClick}
+                        id='saveButton'
+                    >
+                        Save
+                    </Button>
+                </div>
+            </div>  
         );
     }
 }
