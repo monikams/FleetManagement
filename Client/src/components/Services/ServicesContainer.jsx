20602719a96765771c8b983565fca6bc50d4d564
monika_spasova <monikaspasova1@gmail.com>
2018-12-28 14:59:03 +0200

    Updated the Services table and the BE models

---
+++ Client/src/components/Services/ServicesContainer.jsx
@@ -0,0 +1,144 @@
+import React, { Component } from 'react';
+import PropTypes from "prop-types";
+import Immutable from 'immutable';
+import DriversStore from '../../stores/DriversStore';
+import CompaniesStore from '../../stores/CompaniesStore';
+import DriversActions from '../../actions/DriversActions.js';
+import CompaniesActions from '../../actions/CompaniesActions.js';
+import connectToStores from 'alt-utils/lib/connectToStores';
+import shallowEqual from 'shallowequal';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import DeleteIcon from '@material-ui/icons/Delete';
+import EditIcon from '@material-ui/icons/Edit';
+import Button from '@material-ui/core/Button';
+import { withRouter } from 'react-router';
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+  },
+  button: {
+      marginTop: '15px', 
+  }
+});
+
+class ServicesContainer extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    static getStores() {
+        return [CompaniesStore, DriversStore];
+    }
+
+    static getPropsFromStores() {
+        return {
+            drivers: DriversStore.getDrivers(), 
+        }
+    }
+
+    componentWillMount() {
+        const { params: { companyId } } = this.props;
+        DriversActions.loadDrivers(companyId);
+    }
+
+    componentWillUnmount() {
+        DriversActions.unloadDrivers();
+    }
+
+    shouldComponentUpdate = (nextProps, nextState) => !shallowEqual(this.props, nextProps) || !shallowEqual(this.state, nextState);
+
+    handleEditClick(driverId) {
+        const { params: { companyId } } = this.props;
+        this.props.router.replace(`/companies/${companyId}/editDriver/${driverId}`);
+    };
+
+    handleDeleteClick(driverId) {
+        const { params: { companyId } } = this.props;
+        DriversActions.deleteDriver(driverId, companyId);
+    };
+
+    handleCreateDriverClick = () => {
+        const { params: { companyId } } = this.props;
+        this.props.router.replace(`/companies/${companyId}/createDriver`);
+    };
+
+    render() {      
+        const { drivers, classes } = this.props;
+        
+        return (
+            <div>
+                <Button 
+                    variant="contained" 
+                    size="large" 
+                    color="primary" 
+                    className={classes.button}
+                    onClick={this.handleCreateDriverClick}
+                    id='loginButton'
+                >
+                    Create driver
+                </Button>
+                <Paper className={classes.root}>
+                    <Table className={classes.table}>
+                        <TableHead>
+                        <TableRow>
+                            <TableCell>Name</TableCell>
+                            <TableCell>Description</TableCell>
+                            <TableCell>Created</TableCell>
+                            <TableCell>Mileage Rule</TableCell>
+                            <TableCell>Next Service Mileage</TableCell>
+                            <TableCell>Mileage Reminder</TableCell>
+                            <TableCell>Time Rule</TableCell>
+                            <TableCell>Next Service Time</TableCell>
+                            <TableCell>Time Reminder</TableCell>
+                        </TableRow>
+                        </TableHead>
+                        <TableBody>
+                        {drivers.map(driver => {
+                            return (
+                            <TableRow key={driver.Id}>
+                                <TableCell component="th" scope="row">
+                                {driver.Name}
+                                </TableCell>
+                                <TableCell>{driver.Address}</TableCell>
+                                <TableCell>{driver.Email}</TableCell>
+                                <TableCell>{driver.Telephone}</TableCell>
+                                <TableCell><EditIcon onClick={() => this.handleEditClick(driver.Id)}/></TableCell>
+                                <TableCell><DeleteIcon onClick={() => this.handleDeleteClick(driver.Id)} /></TableCell>
+                            </TableRow>
+                            );
+                        })}
+                        </TableBody>
+                    </Table>
+                </Paper>
+            </div>
+        );
+    }
+}
+
+ServicesContainer.propTypes = {
+    drivers: PropTypes.instanceOf(Immutable.Iterable),
+    companies: PropTypes.instanceOf(Immutable.Iterable),
+    classes: PropTypes.object.isRequired,
+    params: PropTypes.object.isRequired,
+};
+
+ServicesContainer.defaultProps = {
+    drivers: Immutable.List(),
+    companies: Immutable.List(),
+    params: {},
+};
+
+export default withStyles(styles)(withRouter(connectToStores(ServicesContainer)));
--- Server/DataAccessService/Models/Service.cs
+++ Server/DataAccessService/Models/Service.cs
@@ -10,19 +10,26 @@ namespace DataAccessService.Models
 
         public string Description { get; set; }
 
-        public DateTimeOffset Created { get; set; }
+        public DateTimeOffset? Created { get; set; }
 
+        public int? TimeRule { get; set; }
+
+        // 1 => Day; 2 => Months; 3 => Years
+        public int? TimeRuleEntity { get; set; }
+
+        public int? TimeReminder { get; set; }
+
+        // 1 => Day; 2 => Months; 3 => Years
+        public int? TimeReminderEntity { get; set; }
+
+        public DateTimeOffset? NextServiceTime { get; set; }
+
+        // Mileage related properties
         public int? MileageRule { get; set; }
 
-        public DateTimeOffset? TimeRule { get; set; }
-
-        public DateTimeOffset? NextServiceMileage { get; set; }
-
-        public int? NextServiceTime { get; set; }
-
-        public DateTimeOffset? TimeReminder { get; set; }
-
         public int? MileageReminder { get; set; }
+
+        public int? NextServiceMileage { get; set; }
 
         public string VehicleId { get; set; }
 
