bb0142fe73a2807d0085fc2a63f8b4319452aef5
monika_spasova <monikaspasova1@gmail.com>
2019-01-05 11:54:21 +0200

    Implemented MailHelper class with send email functionality

---
+++ Server/Infrastructure/Helpers/MailHelper.cs
@@ -0,0 +1,25 @@
+﻿namespace Infrastructure.Helpers
+{
+    using System.Net;
+    using System.Net.Mail;
+    using System.Threading.Tasks;
+
+    public class MailHelper
+    {
+        public static async Task SendEmail(string recipients, string subject, string body)
+        {
+            var userName = "fleet.management.official@gmail.com";
+            var password = "++!Fl$$t.M@n@gm$nt!";
+
+            var credentials = new NetworkCredential(userName, password);
+            var client = new SmtpClient("smtp.gmail.com", 587)
+                             {
+                                 Credentials = credentials,
+                                 EnableSsl = true
+                             };
+
+            var mailMessage = new MailMessage(userName, recipients, subject, body);
+            await client.SendMailAsync(mailMessage);
+        }
+    }
+}
