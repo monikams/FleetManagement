21df5c7bc57aa41e536dd7273b894a36ec8ebdba
monika_spasova <monikaspasova1@gmail.com>
2019-05-03 12:25:12 +0300

    Added email validation message to the registration form

--- Client/src/components/Authorization/RegistrationContainer.jsx
+++ Client/src/components/Authorization/RegistrationContainer.jsx
@@ -11,6 +11,7 @@ import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import '../../styles/AuthorizationContainer.css';
 import { isFieldValid, isButtonDisabled } from '../../utils/validation.js';
+import TextfieldValidationMessage from '../common/TextfieldValidationMessage.jsx'
 
 const styles = theme => ({
   button: {
@@ -47,6 +48,7 @@ class RegistrationContainer extends Component {
             isValid: {
                 'username': true,
                 'email': true,
+                'validEmail': true,
                 'password': true,
                 'confirmPassword': true,
             },
@@ -61,6 +63,24 @@ class RegistrationContainer extends Component {
         this.setState({ localUser: newUser });
     };
 
+    handleBlur = name => event => {
+        const { target: { value }} = event;     
+        const { localUser, isValid } = this.state;
+        const emailRegExpression = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+        if (name === 'validEmail') {
+            isValid[name] = emailRegExpression.test(String(value).toLowerCase());
+            let newUser;
+            if (!isValid[name]) {
+                 newUser = merge(localUser, { [name]: '' });    
+            } else {
+                 newUser = merge(localUser, { [name]: value }); 
+            }
+
+             this.setState({ localUser: newUser });
+        }    
+    };
+
     handleRegisterButtonClick = () => {
         AuthorizationActions.registerUser(this.state.localUser);
     }
@@ -72,68 +92,70 @@ class RegistrationContainer extends Component {
     return (
       <div className={classes.form} >  
         <div className={classes.container} >
-        <TextField
-            required
-            fullWidth
-            error={!isFieldValid('username',isValid)}
-            autoComplete="off"
-            InputLabelProps={{
-                shrink: true,
-            }}
-            id="username"
-            label="Username"
-            placeholder="Enter your username"
-            className={classes.textField}          
-            onChange={this.handleChange('username')}
-            margin="normal"
-            />
-        <TextField
-            required
-            fullWidth
-            error={!isFieldValid('email',isValid)}
-            autoComplete="off"
-            InputLabelProps={{
-                shrink: true,
-            }}
-            id="email"
-            label="Email"
-            placeholder="Enter your email"
-            className={classes.textField}         
-            onChange={this.handleChange('email')}
-            margin="normal"
+           <TextField
+                required
+                fullWidth
+                error={!isFieldValid('username',isValid)}
+                autoComplete="off"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                id="username"
+                label="Username"
+                placeholder="Enter your username"
+                className={classes.textField}          
+                onChange={this.handleChange('username')}
+                margin="normal"
             />
             <TextField
-            required
-            fullWidth
-            error={!isFieldValid('password',isValid)}
-            autoComplete="off"
-            InputLabelProps={{
-                shrink: true,
-            }}
-            id="password"
-            label="Password"
-            placeholder="Enter your password"
-            className={classes.textField}
-            type="password"
-            autoComplete="current-password"
-            onChange={this.handleChange('password')}
-            margin="normal"
+                required
+                fullWidth
+                error={!isFieldValid('email',isValid) || !isFieldValid('validEmail',isValid)}
+                autoComplete="off"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                id="email"
+                label="Email"
+                placeholder="Enter your email"
+                className={classes.textField}         
+                onChange={this.handleChange('email')}
+                onBlur={this.handleBlur('validEmail')}
+                margin="normal"
+            />
+            {!isValid['validEmail'] && <TextfieldValidationMessage message="Please enter a valid email!" />}
+            <TextField
+                required
+                fullWidth
+                error={!isFieldValid('password',isValid)}
+                autoComplete="off"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                id="password"
+                label="Password"
+                placeholder="Enter your password"
+                className={classes.textField}
+                type="password"
+                autoComplete="current-password"
+                onChange={this.handleChange('password')}
+                margin="normal"
             />
             <TextField
-            required
-            fullWidth
-            error={!isFieldValid('confirmPassword',isValid)}
-            autoComplete="off"
-            InputLabelProps={{
-                shrink: true,
-            }}
-            id="confirm-password"
-            label="Confirm password"
-            placeholder="Confirm your password"
-            className={classes.textField}
-            type="password"
-            onChange={this.handleChange('confirmPassword')}
-            margin="normal"
+                required
+                fullWidth
+                error={!isFieldValid('confirmPassword',isValid)}
+                autoComplete="off"
+                InputLabelProps={{
+                    shrink: true,
+                }}
+                id="confirm-password"
+                label="Confirm password"
+                placeholder="Confirm your password"
+                className={classes.textField}
+                type="password"
+                onChange={this.handleChange('confirmPassword')}
+                margin="normal"
             />
         </div>
         <div className={classes.container} >
---
+++ Client/src/components/common/TextfieldValidationMessage.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import Button from '@material-ui/core/Button';
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import ErrorIcon from '@material-ui/icons/Error';
+import InfoIcon from '@material-ui/icons/Info';
+import CloseIcon from '@material-ui/icons/Close';
+import green from '@material-ui/core/colors/green';
+import amber from '@material-ui/core/colors/amber';
+import IconButton from '@material-ui/core/IconButton';
+import Snackbar from '@material-ui/core/Snackbar';
+import SnackbarContent from '@material-ui/core/SnackbarContent';
+import WarningIcon from '@material-ui/icons/Warning';
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+  emailValidationMessage: {
+    color: '#F44336',
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    padding: 0,
+    fontSize: '0.8rem',
+    fontFamily: 'Roboto, Helvetica, Arial, sans-serif',
+    lineHeight: 1,
+  }
+});
+
+function TextfieldValidationMessage(props) {
+  const { classes, message } = props;
+
+  return (
+    <p id="email-error-text" className={classes.emailValidationMessage} >Please enter a valid email!</p>
+  );
+}
+
+TextfieldValidationMessage.propTypes = {
+  message: PropTypes.string.isRequired,
+};
+
+TextfieldValidationMessage.defaultProps = {
+    message: 'Error!',
+};
+
+export default withStyles(styles)(TextfieldValidationMessage);
