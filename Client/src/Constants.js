2ffe7c0ab709d6384a94ac2543d5d45b474f4c20
monika_spasova <monikaspasova1@gmail.com>
2019-06-09 15:30:43 +0300

    Added apiURL of deployed BE

--- Client/src/Constants.js
+++ Client/src/Constants.js
@@ -1,4 +1,5 @@
 const baseURL = 'http://localhost:3000';
-const apiURL = 'http://localhost:19631';
+// const apiURL = 'http://localhost:19631';
+const apiURL = 'http://fleetmanagement.apphb.com/';
 
 export { baseURL, apiURL };
